ebe0de1f3e943a5b8c05251cb5c980b988f5e511
durgesh2293 <durgeshpatil2293@gmail.com>
2026-08-11 17:50:41 +0530

    Post Dotnet added

---
+++ backend/amigos_dotnet_backend/Models/Entities/Comment.cs
@@ -0,0 +1,40 @@
+﻿using SocialMediaAdminBackend.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SocialMediaAdminBackend.Models.Entities;
+
+[Table("comments")]
+public class Comment
+{
+    [Key]
+    [Column("id")]
+    public long Id { get; set; }
+
+    [Column("post_id")]
+    public long PostId { get; set; }
+
+    [Column("user_id")]
+    public long UserId { get; set; }
+
+    [Column("content")]
+    public string Content { get; set; } = string.Empty;
+
+    [Column("parent_id")]
+    public long? ParentId { get; set; }
+
+    [Column("is_edited")]
+    public bool IsEdited { get; set; }
+
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; }
+
+    [Column("updated_at")]
+    public DateTime UpdatedAt { get; set; }
+
+    [ForeignKey("PostId")]
+    public Post? Post { get; set; }
+
+    [ForeignKey("UserId")]
+    public User? User { get; set; }
+}
---
+++ backend/amigos_dotnet_backend/Models/Entities/Follow.cs
@@ -0,0 +1,31 @@
+﻿using SocialMediaAdminBackend.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SocialMediaAdminBackend.Models.Entities;
+
+[Table("follows")]
+public class Follow
+{
+    [Key]
+    [Column("id")]
+    public long Id { get; set; }
+
+    [Column("follower_id")]
+    public long FollowerId { get; set; }
+
+    [Column("following_id")]
+    public long FollowingId { get; set; }
+
+    [Column("status")]
+    public string Status { get; set; } = "PENDING";
+
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; }
+
+    [ForeignKey("FollowerId")]
+    public User? Follower { get; set; }
+
+    [ForeignKey("FollowingId")]
+    public User? Following { get; set; }
+}
